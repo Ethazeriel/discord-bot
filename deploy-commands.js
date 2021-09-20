389b629225a5e3e9b08c98a19b71225cb9865ad9
Neil Merchant <njlmerchant@gmail.com>
2021-09-19 22:18:01 -0700

    added ability to push global commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,10 +2,15 @@ const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
-
+const launchArg = process.argv[2];
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+if (process.argv.length > 3) {
+  console.log('Too many arguments');
+  process.exit();
+}
+console.log('Deployment started with scope: ' + launchArg);
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   commands.push(command.data.toJSON());
@@ -15,12 +20,22 @@ const rest = new REST({ version: '9' }).setToken(token);
 
 (async () => {
   try {
-    await rest.put(
-      Routes.applicationGuildCommands(clientId, guildId),
-      { body: commands },
-    );
+    if (launchArg == 'guild') {
+      await rest.put(
+        Routes.applicationGuildCommands(clientId, guildId),
+        { body: commands },
+      );
+      console.log('Successfully registered commands at scope: guild');
+    } else if (launchArg == 'global') {
+      await rest.put(
+        Routes.applicationCommands(clientId),
+        { body: commands },
+      );
+      console.log('Successfully registered commands at scope: guild');
+    } else {
+      console.log('Invalid launch argument. The valid options are: guild, global');
+    }
 
-    console.log('Successfully registered application commands.');
   } catch (error) {
     console.error(error);
   }
