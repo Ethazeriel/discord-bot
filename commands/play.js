66756d30ebeaedb55ca82ac7ada2e9e35435cc80
Neil Merchant <njlmerchant@gmail.com>
2021-09-21 21:51:46 -0700

    initial youtube integration

--- commands/play.js
+++ commands/play.js
@@ -1,14 +1,41 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, entersState, VoiceConnectionStatus } = require('@discordjs/voice');
+const ytdl = require('ytdl-core');
 
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('play')
-    .setDescription('play a song'),
+    .setDescription('play a song')
+    .addStringOption(option =>
+      option.setName('source').setDescription('Song source').setRequired(true)),
 
 
   async execute(interaction) {
+
+    const reqstr = interaction.options.getString('source');
+
+    let songInfo = null;
+    let song = null;
+    switch (getRequestType(reqstr)) {
+    case 'youtube':
+      // handle direct youtube urls
+      console.log(reqstr);
+      songInfo = await ytdl.getInfo(reqstr);
+      song = {
+        title: songInfo.videoDetails.title,
+        url: songInfo.videoDetails.video_url,
+      };
+      break;
+    case 'spotify':
+      // handle direct spotify urls
+      break;
+    default:
+      // error out if we don't know how to handle the string
+      console.log(`Failed to parse string ${reqstr}`);
+      await interaction.reply({ content:`Failed to parse string ${reqstr}`, ephemeral: true });
+    }
+
     const connection = joinVoiceChannel({
       channelId: interaction.member.voice.channel.id,
       guildId: interaction.member.voice.channel.guild.id,
@@ -16,12 +43,13 @@ module.exports = {
     });
     connection.on('stateChange', (oldState, newState) => { console.log(`Connection transitioned from ${oldState.status} to ${newState.status}`); });
     const player = createAudioPlayer();
-    player.on('error', error => {console.error('error:', error.message, 'with file', error.resource.metadata.title);});
+    player.on('error', error => {console.error('error:', error.message, 'with file', error.resource.metadata.title, 'full:', error);});
     player.on('stateChange', (oldState, newState) => { console.log(`Player transitioned from ${oldState.status} to ${newState.status}`); });
 
-    const resource = createAudioResource('space.mp3', { metadata: { title: 'SPACE' } });
+    const resource = createAudioResource(ytdl(song.url), { metadata: { title: song.title } });
     player.play(resource);
     connection.subscribe(player);
+
     try {
       await entersState(connection, VoiceConnectionStatus.Ready, 5e3);
       await interaction.reply(`Now Playing: ${resource.metadata.title}`);
@@ -34,3 +62,11 @@ module.exports = {
   },
 };
 
+function getRequestType(string) {
+  const test = string.split('//');
+  if (test[1].startsWith('www.youtube' || 'youtube' || 'youtu.be')) {
+    return ('youtube');
+  } else if (test[1].startsWith('open.spotify' || 'spotify')) {
+    return ('spotify');
+  }
+}
