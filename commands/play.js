978a26a75c08924f2bae351bb8a847d67698a3f8
Neil Merchant <njlmerchant@gmail.com>
2021-10-03 19:36:18 -0700

    command restructure

---
+++ commands/play.js
@@ -0,0 +1,119 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const music = require('../music.js');
+const ytdl = require('ytdl-core');
+const utils = require('../utils.js');
+const playlists = require('../playlists.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('play')
+    .setDescription('Play something!')
+    .addStringOption(option =>
+      option.setName('url').setDescription('Youtube URL').setRequired(true))
+    .addStringOption(option =>
+      option.setName('type').setDescription('play type')
+        .addChoice('End of queue', 'end')
+        .addChoice('Top of queue', 'top')
+        .addChoice('Skip current', 'skip')
+        .addChoice('Trainsong', 'trainsong')),
+
+
+  async execute(interaction) {
+
+    switch (interaction.options.getString('type')) {
+
+    case null || 'end': {
+      const reqstr = interaction.options.getString('url');
+
+      let trackInfo = null;
+      let track = null;
+      try {
+        // handle direct youtube urls
+        trackInfo = await ytdl.getInfo(reqstr);
+        track = {
+          title: trackInfo.videoDetails.title,
+          artist: 'placeholder artist',
+          album: 'placeholder album',
+          url: trackInfo.videoDetails.video_url,
+          albumart: 'albumart/albumart.jpg',
+        };
+      } catch (error) {
+        console.error('Error parsing youtube string:', reqstr, '. Stacktrace:', error);
+        await interaction.reply({ content:`Error parsing youtube string: ${reqstr}`, ephemeral: true });
+      }
+
+      music.createVoiceConnection(interaction);
+      music.addToQueue(track);
+      utils.generateTrackEmbed(interaction, track, 'Added to Queue: ');
+      break;
+    }
+
+    case 'top': {
+      const reqstr = interaction.options.getString('url');
+
+      let trackInfo = null;
+      let track = null;
+      try {
+        // handle direct youtube urls
+        trackInfo = await ytdl.getInfo(reqstr);
+        track = {
+          title: trackInfo.videoDetails.title,
+          artist: 'placeholder artist',
+          album: 'placeholder album',
+          url: trackInfo.videoDetails.video_url,
+          albumart: 'albumart/albumart.jpg',
+        };
+      } catch (error) {
+        console.error('Error parsing youtube string:', reqstr, '. Stacktrace:', error);
+        await interaction.reply({ content:`Error parsing youtube string: ${reqstr}`, ephemeral: true });
+      }
+
+      music.createVoiceConnection(interaction);
+      music.addToQueueTop(track);
+      utils.generateTrackEmbed(interaction, track, 'Added to top of queue: ');
+      break;
+    }
+
+    case 'skip': {
+      const reqstr = interaction.options.getString('url');
+
+      let trackInfo = null;
+      let track = null;
+      try {
+        // handle direct youtube urls
+        trackInfo = await ytdl.getInfo(reqstr);
+        track = {
+          title: trackInfo.videoDetails.title,
+          artist: 'placeholder artist',
+          album: 'placeholder album',
+          url: trackInfo.videoDetails.video_url,
+          albumart: 'albumart/albumart.jpg',
+        };
+      } catch (error) {
+        console.error('Error parsing youtube string:', reqstr, '. Stacktrace:', error);
+        await interaction.reply({ content:`Error parsing youtube string: ${reqstr}`, ephemeral: true });
+      }
+
+      music.createVoiceConnection(interaction);
+      music.addToQueueSkip(track);
+      utils.generateTrackEmbed(interaction, track, 'Playing Now: ');
+      break;
+    }
+
+    case 'trainsong': {
+      music.createVoiceConnection(interaction);
+      await music.addMultipleToQueue(playlists.trainsong);
+      // utils.generateQueueEmbed(interaction, music.getCurrentTrack(), music.queue, 'Queued Trainsong:', 1);
+      await interaction.reply({ content:'TRAINSONG' });
+      break;
+    }
+
+    default: {
+      console.log('OH NO SOMETHING\'S FUCKED');
+      await interaction.reply({ content:'Something broke. Please try again', ephemeral: true });
+    }
+
+    }
+
+  },
+};
---
+++ commands/queue.js
@@ -0,0 +1,85 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const music = require('../music.js');
+const utils = require('../utils.js');
+
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('queue')
+    .setDescription('queue related functions')
+    .addSubcommand(subcommand => subcommand
+      .setName('skip')
+      .setDescription('skips the currently running song'))
+    .addSubcommand(subcommand => subcommand
+      .setName('show')
+      .setDescription('Prints the current queue')
+      .addIntegerOption(option =>
+        option.setName('page').setDescription('Page to show').setRequired(false)))
+    .addSubcommand(subcommand => subcommand
+      .setName('remove')
+      .setDescription('remove a track from the queue')
+      .addIntegerOption(option =>
+        option.setName('track').setDescription('Track to remove').setRequired(true)))
+    .addSubcommand(subcommand => subcommand
+      .setName('loop')
+      .setDescription('Toggles queue looping')),
+
+
+  async execute(interaction) {
+
+    switch (interaction.options.getSubcommand()) {
+
+    case 'skip': {
+
+      let track = null;
+
+      track = {
+        title: 'Silence',
+        artist: 'Eth',
+        album: 'ethsound',
+        url: '../empty.mp3',
+        albumart: 'albumart/albumart.jpg',
+      };
+
+      music.createVoiceConnection(interaction);
+      music.playLocalTrack(track);
+      await interaction.reply({ content:'Skipped.' });
+      break;
+    }
+
+    case 'showqueue': {
+      const track = music.getCurrentTrack();
+      let page = interaction.options.getInteger('page');
+      if (page == null) {page = 1;}
+
+      if (track != null) {
+        utils.generateQueueEmbed(interaction, track, music.queue, 'Current Queue:', page);
+      } else {
+        await interaction.reply({ content:'unable to get the current queue.', ephemeral: true });
+      }
+      break;
+    }
+
+    case 'remove': {
+      music.removeTrack(interaction.options.getInteger('track') - 1);
+      await interaction.reply(`Removed item ${interaction.options.getInteger('track')} from the queue.`);
+      break;
+    }
+
+    case 'loop': {
+      const status = music.toggleLoop();
+      if (status == true) {
+        await interaction.reply({ content:'Enabled Queue Loop.' });
+      } else {await interaction.reply({ content:'Disabled Queue Loop.' });}
+      break;
+    }
+
+    default: {
+      console.log('OH NO SOMETHING\'S FUCKED');
+      await interaction.reply({ content:'Something broke. Please try again', ephemeral: true });
+    }
+
+    }
+
+  },
+};
