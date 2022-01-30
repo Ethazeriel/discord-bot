db61fe0b0598702823f401178dde98c4d5dbb72d
Whuppee <Whuppee@users.noreply.github.com>
2022-01-30 10:06:54 -0800

    baseline player dispatch

--- interactions/commands/test.js
+++ interactions/commands/test.js
@@ -1,20 +1,31 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { Player } = require('../player.js');
+const Player = require('../../player.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('test')
-    .setDescription('tests')
+    .setDescription('test')
     .addStringOption(option =>
-      option.setName('test').setDescription('tests').setRequired(true)),
+      option.setName('test').setDescription('test').setRequired(true)),
 
   async execute(interaction) {
-    const test = interaction.options.getString('test');
+    const test = String(interaction.options.getString('test')).toLowerCase();
+
     if (interaction.member?.roles?.cache?.some(role => role.name === 'DJ')) {
       await interaction.deferReply({ ephemeral: true });
 
-      const player = new Player();
-      player.test(interaction, test);
+      const player = await Player.getPlayer(interaction, { explicitJoin : (test == 'join') });
+      if (player) {
+        switch (test) {
+          case 'test': {
+            await player.test(interaction);
+            break;
+          }
+          default: {
+            //
+          }
+        }
+      }
     } else {
       await interaction.reply({ content:'You don\'t have permission to do that.', ephemeral: true });
     }
--- player.js
+++ player.js
@@ -1,16 +1,82 @@
-const { joinVoiceChannel, getVoiceConnection, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
-const { logLine } = require('./logger.js');
 const youtubedl = require('youtube-dl-exec').raw;
+const { joinVoiceChannel, getVoiceConnection, createAudioPlayer, createAudioResource, getVoiceConnections } = require('@discordjs/voice');
+
+const { logLine, logDebug } = require('./logger.js');
 const { useragent } = require('./config.json').youtube;
 
+const players = [];
+
 class Player {
-  constructor() {
+  constructor(interaction) {
+    logDebug('constructor');
+    this.queue = {
+      index: 0,
+      tracks: [],
+      loop: false,
+    };
+
+    this.player = createAudioPlayer();
+    this.player.on('error', error => { logLine('error', [error.stack ]); });
+    this.player.on('stateChange', (oldState, newState) => {
+      logDebug(`Player transitioned from ${oldState.status} to ${newState.status}`);
+
+      if (newState.status == 'idle') { // Starts the next track in line when one finishes
+        //
+      }
+    });
+
+    const connection = joinVoiceChannel({
+      channelId: interaction.member.voice.channel.id,
+      guildId: interaction.member.voice.channel.guild.id,
+      adapterCreator: interaction.member.voice.channel.guild.voiceAdapterCreator,
+    });
+    connection.on('stateChange', (oldState, newState) => {
+      logDebug(`Connection transitioned from ${oldState.status} to ${newState.status}`);
+      if (newState.status == 'destroyed') {
+        //
+      }
+    });
+    connection.subscribe(this.player);
+  }
+
+  test(interaction) {
     //
   }
 
-  async test(interaction, ... input) {
-    await interaction.followUp({ content: `test ${input}`, ephemeral: true });
+  async join(interaction) {
+    joinVoiceChannel({
+      channelId: interaction.member.voice.channelId,
+      guildId: interaction.guild.id,
+      adapterCreator: interaction.member.voice.channel.guild.voiceAdapterCreator,
+    });
+
+    return (this); // lazy code condensing
+  }
+
+  static async getPlayer(interaction, { explicitJoin = false } = {}) {
+    const userChannel = interaction.member.voice.channelId;
+
+    if (!userChannel) {
+      await interaction.followUp({ content:'You must join a voice channel first.', ephemeral: true });
+      return (null);
+    }
+
+    const guild = interaction.guild.id;
+    const connection = getVoiceConnection(guild);
+    const botChannel = (connection) ? interaction.client.channels.cache.get(connection.joinConfig.channelId) : null;
+    const isAlone = botChannel?.members?.size == 1;
+
+    if (userChannel == botChannel?.id) {
+      if (explicitJoin) { await interaction.followUp({ content:'Bot is already in your channel.', ephemeral: true }); }
+      return (players[guild]);
+    } else if (!connection || isAlone) {
+      return (players[guild]?.join(interaction) || (players[guild] = new Player(interaction)));
+    } else {
+      await interaction.followUp({ content:'Bot is busy in another channel.', ephemeral: true });
+      return (null);
+    }
   }
 }
 
+exports.getPlayer = Player.getPlayer;
 exports.Player = Player;
