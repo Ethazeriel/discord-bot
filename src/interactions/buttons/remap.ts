dfc900a26eaf380d05a14faff5e2f561329db60f
Neil Merchant <njlmerchant@gmail.com>
2022-05-26 22:55:36 -0700

    remap button ts conversion

--- src/interactions/buttons/remap.ts
+++ src/interactions/buttons/remap.ts
@@ -1,15 +1,16 @@
 import * as db from '../../database.js';
 import * as utils from '../../utils.js';
 import ytdl from 'ytdl-core';
+import { ButtonInteraction, InteractionUpdateOptions } from 'discord.js';
 
-export const name = 'remap';
+export const name:string = 'remap';
 
-export async function execute(interaction, which) { // button selection function
+export async function execute(interaction:ButtonInteraction, which:string): Promise<void> { // button selection function
   switch (which) {
 
     case 'db': {
-      const match = interaction.message.embeds[0].footer.text.match(/([\w-]{11})([0-3])/);
-      const result = await db.switchAlternate(match[1], match[2]);
+      const match = interaction.message.embeds[0].footer!.text.match(/([\w-]{11})([0-3])/);
+      const result = await db.switchAlternate(match![1], Number(match![2]));
       if (result) {
         const reply = {
           embeds: [
@@ -22,7 +23,7 @@ export async function execute(interaction, which) { // button selection function
           ],
           components:[],
         };
-        await interaction.update(reply);
+        await interaction.update(reply as InteractionUpdateOptions);
       } else {
         const reply = {
           embeds: [
@@ -35,38 +36,38 @@ export async function execute(interaction, which) { // button selection function
           ],
           components:[],
         };
-        await interaction.update(reply);
+        await interaction.update(reply as InteractionUpdateOptions);
       }
       break;
     }
 
     case 'new': {
-      const match = interaction.message.embeds[0].footer.text.match(/([\w-]{11})([\w-]{11})/);
-      const track = await db.getTrack({ 'youtube.id': match[1] });
-      if (!Object.keys(track).length) {
+      const match = interaction.message.embeds[0].footer!.text.match(/([\w-]{11})([\w-]{11})/);
+      const track = await db.getTrack({ 'youtube.id': match![1] });
+      if (!Object.keys(track as object).length) {
         await interaction.followUp({ content:'We don\'t appear to have that track.', ephemeral: true });
         return;
       }
       // const newtrack = await fetch(`https://www.youtube.com/watch?v=${match[2]}`, interaction.id);
-      const newtrack = await db.getTrack({ 'youtube.id': match[2] });
+      const newtrack = await db.getTrack({ 'youtube.id': match![2] });
       let newtube;
-      if (newtrack?.length) {
-        newtube = newtrack.youtube;
+      if (Object.keys(newtrack as object).length) {
+        newtube = newtrack!.youtube;
       } else {
         try {
-          const ytdlResult = await ytdl.getBasicInfo(match[2], { requestOptions: { family:4 } });
+          const ytdlResult = await ytdl.getBasicInfo(match![2], { requestOptions: { family:4 } });
           newtube = {
-            id:match[2],
+            id:match![2],
             name:ytdlResult?.videoDetails.title,
-            art:`https://i.ytimg.com/vi/${match[2]}/hqdefault.jpg`,
+            art:`https://i.ytimg.com/vi/${match![2]}/hqdefault.jpg`,
             duration:Number(ytdlResult?.videoDetails?.lengthSeconds),
           };
-        } catch (error) {
-          await interaction.update({ content:`Error remapping: ${error.message}`, ephemeral: true });
+        } catch (error:any) {
+          await interaction.update({ content:`Error remapping: ${error.message}`, ephemeral: true } as InteractionUpdateOptions);
           return;
         }
       }
-      const query = { 'goose.id':track.goose.id };
+      const query = { 'goose.id':track!.goose.id };
       const update = { $set: { youtube:newtube } };
       await db.updateTrack(query, update);
 
@@ -76,7 +77,7 @@ export async function execute(interaction, which) { // button selection function
             color: 0xd64004,
             author: { name: 'Remapped:', icon_url: utils.pickPride('fish') },
             fields: [
-              { name: 'From:', value: `[${track.youtube.name}](https://youtube.com/watch?v=${track.youtube.id}) - ${new Date(track.youtube.duration * 1000).toISOString().substr(11, 8).replace(/^[0:]+/, '')}` },
+              { name: 'From:', value: `[${track!.youtube.name}](https://youtube.com/watch?v=${track!.youtube.id}) - ${new Date(track!.youtube.duration * 1000).toISOString().substr(11, 8).replace(/^[0:]+/, '')}` },
               { name: 'To:', value: `[${newtube.name}](https://youtube.com/watch?v=${newtube.id}) - ${new Date(newtube.duration * 1000).toISOString().substr(11, 8).replace(/^[0:]+/, '')}` },
             ],
             image: { url: 'attachment://combined.png', height: 0, width: 0 },
@@ -85,7 +86,7 @@ export async function execute(interaction, which) { // button selection function
         components:[],
         files: [],
       };
-      await interaction.update(reply);
+      await interaction.update(reply as InteractionUpdateOptions);
       break;
     }
 
@@ -100,7 +101,7 @@ export async function execute(interaction, which) { // button selection function
         ],
         components:[],
       };
-      await interaction.update(reply);
+      await interaction.update(reply as InteractionUpdateOptions);
       break;
     }
 
