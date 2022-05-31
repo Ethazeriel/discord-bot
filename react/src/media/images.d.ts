4a526ee4d0c316f6a0607a182f30a90feffff24f
Whuppee <Whuppee@users.noreply.github.com>
2022-05-30 22:04:41 -0700

    oh no

--- react/src/media/images.d.ts
+++ react/src/media/images.d.ts
@@ -1,2 +1,10 @@
 declare module '*.png';
 declare module '*.jpg';
+// declare module '*.svg';
+
+declare module '*.svg' {
+  import React from 'react';
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>; // React.SVGProps<SVGSVGElement>
+  const src: string;
+  export default src;
+}
