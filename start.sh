9c19743319ffcdd7339021320bee76b27998b2f6
Neil Merchant <njlmerchant@gmail.com>
2023-02-18 12:50:30 -0800

    proper nvm maybe

--- start.sh
+++ start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 cd client
 nvm use
 npm i
 npm run build
+
 cd ../server
 nvm use
-npm i
-tsc
+npm install
+npm run build
 cd ..
 node server/build/index
