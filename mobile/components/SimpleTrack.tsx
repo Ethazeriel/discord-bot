738fb82f230190bd16189d8f8c16ad241da8f707
Neil Merchant <njlmerchant@gmail.com>
2024-10-17 19:19:15 -0700

    unduplicate code, style

--- mobile/components/SimpleTrack.tsx
+++ mobile/components/SimpleTrack.tsx
@@ -1,41 +1,52 @@
 import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 import { Image } from 'expo-image';
 import { StyleSheet, View } from 'react-native';
 import { timeDisplay } from '@/components/utils';
+import styled from 'styled-components/native';
+import React from 'react';
 
-export function SimpleTrackFromTrack({source, id}:{source:Track, id:number}) {
+export function SimpleTrack({source, id}:{source:Track, id:number}): React.JSX.Element | null
+export function SimpleTrack({source, id}:{source:TrackSource, id:number}): React.JSX.Element | null 
+export function SimpleTrack({source, id}:{source:any, id:number}): React.JSX.Element | null {
   if (Object.keys(source).length) {
-    return (
-      <View style={styles.trackStyle}>
-        <Image style={styles.image} source={source.goose.track.art} contentFit="cover" transition={1000} />
-        <View style={styles.numContainer}>
-          <ThemedText type="defaultSemiBold">{(id + 1)}</ThemedText>
-        </View>
-        <View style={styles.details}>
-          <ThemedText type="subtitle">{source.goose.track.name}</ThemedText>
-          <ThemedText type="default">{source.goose.artist.name} - {source.goose.album.name}</ThemedText>
-        </View>
-        <ThemedText style={styles.playtime}>{timeDisplay(source.goose.track.duration)}</ThemedText>
-      </View>
-    );
-  } else {return null;}
-}
 
-export function SimpleTrackFromSource({source, id}:{source:TrackSource, id:number}) {
-  if (Object.keys(source).length) {
+  const track = {
+    art:'',
+    name:'',
+    artist:'',
+    album:'',
+    duration:0,
+  };
+
+    if ( source.goose ) {
+      // source is Track
+      track.art = source.goose.track.art;
+      track.name = source.goose.track.name;
+      track.artist = source.goose.artist.name;
+      track.album = source.goose.album.name;
+      track.duration = source.goose.track.duration;
+    } else {
+      // source is TrackSource
+      track.art = source.art;
+      track.name = source.name;
+      track.artist = source.artist.name;
+      track.album = source.album.name;
+      track.duration = source.duration;
+    }
+
+
     return (
-      <View style={styles.trackStyle}>
-        <Image style={styles.image} source={source.art} contentFit="cover" transition={1000} />
-        <View style={styles.numContainer}>
+      <Track style={ id%2 ? styles.even : styles.odd}>
+        <Image style={styles.image} source={track.art} contentFit="cover" transition={1000} />
+        <VertCon>
           <ThemedText type="defaultSemiBold">{(id + 1)}</ThemedText>
-        </View>
-        <View style={styles.details}>
-          <ThemedText type="subtitle">{source.name}</ThemedText>
-          <ThemedText type="default">{source.artist.name} - {source.album.name}</ThemedText>
-        </View>
-        <ThemedText style={styles.playtime}>{timeDisplay(source.duration)}</ThemedText>
-      </View>
+        </VertCon>
+        <Details>
+          <ThemedText type="subtitle">{track.name}</ThemedText>
+          <ThemedText type="default">{track.artist} - {track.album}</ThemedText>
+        </Details>
+        <Timer>{timeDisplay(track.duration)}</Timer>
+      </Track>
     );
   } else {return null;}
 }
@@ -48,32 +59,51 @@ const styles = StyleSheet.create({
     marginLeft: 5,
     marginRight: 5
   },
-  trackStyle: {
-    flexDirection: 'row',
-    height: 70,
-    width: "100%",
-    alignItems: 'center',
-    backgroundColor: '#242627',
-    
-
+  even: {
+    backgroundColor:'#333536'
   },
-  numContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    marginLeft: 5,
-    backgroundColor: '#242627',
-  },
-  details: {
-    margin: 0,
-    flex: 15,
-    textAlign: 'left',
-    overflow: 'hidden',
-    backgroundColor: '#242627',
-  },
-  playtime: {
-    fontSize: 16,
-    lineHeight: 24,
-    fontWeight: '600',
-    marginRight: 5,
-  },
+  odd: {
+    backgroundColor:'#242627'
+  }
 });
+
+const Art = styled.Image`
+  height:6.4vh;
+  width:6.4vh;
+  background-color:#00868633;
+  margin-left:0.5%;
+  margin-right:0.5%;
+  object-fit:cover;
+  transition:1000ms;
+`; // TODO - can't define image source if we use this
+const Track = styled.View`
+  display:flex;
+  flex-direction:row;
+  height:80px;
+  width: 100%;
+  align-items:center;
+  background-color:#242627;
+  /* &:nth-child() {background-color: #223842;} */
+`;
+const VertCon = styled.View`
+  display:flex;
+  flex-direction:column;
+  margin-left:0.5em;
+  margin-right:1%;
+  /* background-color:#242627; */
+`;
+const Details = styled.View`
+  margin:0px;
+  width:30vw;
+  flex:15;
+  text-align:left;
+  overflow:hidden;
+  /* background-color:#242627; */
+`;
+const Timer = styled.Text`
+  font-size:16px;
+  line-height:24px;
+  font-weight:bold;
+  margin-right:5px;
+  color:#ECEDEE;
+`;
