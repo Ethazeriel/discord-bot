ce09cb6fe5b8dcad9434cf74076422a78af6bd58
Whuppee <47512674+Whuppee@users.noreply.github.com>
2024-12-26 23:36:47 -0800

    new regexes

--- client/src/regexes.js
+++ client/src/regexes.js
@@ -2,5 +2,6 @@ export const youtubePattern = /(?:youtube\.com|youtu\.be)(\/(?:[\w-]+\?v=|embed\
 export const youtubePlaylistPattern = /(?:youtube\.com)(\/(?:playlist\?list=)?)([\w-]{34})(\S+)?/;
 export const spotifyPattern = /(?:spotify\.com|spotify)(?:\/|:)((?:track|playlist|album){1})(?:\/|:)([a-zA-Z0-9]{22})/;
 export const napsterPattern = /(?:play\.napster\.com)(?:\/album\/|\/playlist\/)((?:alb\.|pp\.|mp\.)(?:\d{9}){1}).*((?<=&rsrc=)(?:track|playlist|album){1})(?:&trackId=)?((?:tra\.\d{9}))?/;
+export const subsonicPattern = /(?:\/app\/#\/)((?:track|playlist|album){1})(?:\/)([a-f0-9-]{32,36})/;
 
 export const dragPattern = /(url)/i;
